4607536680d406d5e6df0456cda071b3d7111d4a
fabianagenila <fabianagenila@gmail.com>
2023-02-22 12:47:16 -0300

    adding counter hook

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
 import ChangeCounter from "../components/ChangeCounter";
+import { useCounterContext } from "../hook/useCounterContext";
 
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
 
   return (
     <div>
