4294faf156c53e9682ec89054f32bfccf2c0bf54
fabianagenila <fabianagenila@gmail.com>
2023-02-22 14:21:31 -0300

    context dispatch and deleting useMemo

--- src/context/TitleColorContext.jsx
+++ src/context/TitleColorContext.jsx
@@ -1,17 +1,23 @@
-import { createContext, useMemo, useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 export const TitleColorContext = createContext();
 
 export const titleColorReducer = (state, action) => {
-  //switch
+  switch (action.type) {
+    case "RED":
+      return { ...state, color: "red" };
+    case "BLUE":
+      return { ...state, color: "blue" };
+    default:
+      return state;
+  }
 };
 
 export const TitleColorContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(titleColorReducer, { color: "purple" });
-  const value = useMemo(() => ({ ...state }), []);
 
   return (
-    <TitleColorContext.Provider value={value}>
+    <TitleColorContext.Provider value={{ ...state, dispatch }}>
       {children}
     </TitleColorContext.Provider>
   );
